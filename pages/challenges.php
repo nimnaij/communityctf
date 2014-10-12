9d7675e95fe1565f006ea2ce42699d5312ea736f
jianmin <jianmin@gmail.com>
2014-10-12 10:35:51 -0700

    Tweaked Challenges UI.

--- pages/challenges.php
+++ pages/challenges.php
@@ -13,9 +13,31 @@ if (session_id() == '') {
 <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 <script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript">
-
-$(function() {
 ANIMATION_TIME = 200;
+function updateSelectors() {
+  $(".selector span").each(function() {
+    if($(".challenge div.class:contains('"+$(this).text()+"')").parent().parent().is(":visible")) {
+      $(this).removeClass("off").addClass("on");
+    } else {
+      $(this).removeClass("off").addClass("off");
+    }
+    if(!$(".challenge div.class:contains('"+$(this).text()+"')").length) {
+      $(this).hide();
+    }
+  });
+}
+function collapseAll() {
+  $(".challenge div.details").hide(ANIMATION_TIME);
+}
+function expandAll() {
+  $(".challenge div.details").show(ANIMATION_TIME);
+}
+function showAll() {
+  $(".challenge").show(ANIMATION_TIME);
+  updateSelectors();
+}
+$(function() {
+  updateSelectors();
   $(".selector span").addClass("on");
   $(".challenge div.title").click(function() {
     $( this ).siblings(".details").toggle(ANIMATION_TIME);
@@ -24,10 +46,10 @@ ANIMATION_TIME = 200;
     selection = $( this ).text();
     if($(this).hasClass("off")) {
       $(this).removeClass("off").addClass("on");
-      $(".challenge:contains('"+selection+"')").show(ANIMATION_TIME);
+      $(".challenge div.class:contains('"+selection+"')").parent().parent().show(ANIMATION_TIME);
     } else { 
       $(this).removeClass("off").addClass("off");
-      $(".challenge:contains('"+selection+"')").hide(ANIMATION_TIME);
+      $(".challenge div.class:contains('"+selection+"')").parent().parent().hide(ANIMATION_TIME);
     }
 
 
@@ -39,7 +61,7 @@ ANIMATION_TIME = 200;
 <body>
 <?php include("navigation.html"); ?>
 <div class="content">
-  <div class="selector"><span>Web</span><span>Reversing</span><span>Exploitation</span><span>Crypto</span><span>Binary</span><span>Forensics</span><span>Networking</span></div>
+  <div class="selector"><span>Web</span><span>Reversing</span><span>Exploitation</span><span>Crypto</span><span>Binary</span><span>Forensics</span><span>Networking</span><a href="javascript:collapseAll();">Collapse All</a> <a href="javascript:expandAll();">Expand All</a> <a href="javascript:showAll();">Show All</a></div>
   <div class="challenges">
     <div class="challenge">
       <div class="title">ChalName | Author | Points | SolvedCount <div class="class">Web</div></div>
@@ -59,6 +81,10 @@ ANIMATION_TIME = 200;
     </div>
     <div class="challenge">
       <div class="title">ChalName | Author | Points | SolvedCount <div class="class">Forensics</div></div>
+      <div class="details">Description Here<br />more description</div>
+    </div>
+    <div class="challenge">
+      <div class="title">ChalName | Author | Points | SolvedCount <div class="class">Networking Forensics</div></div>
       <div class="details">Description Here<br />more description</div>
     </div>
   </div>
